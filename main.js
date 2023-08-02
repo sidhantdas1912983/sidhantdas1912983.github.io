daef02c493d56edbd43d141ba7e6c74ae0055b32
Sidhant <sidhantdas1912973@gmail.com>
2023-08-01 18:08:51 -0700

    removed image5

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const express  = require('express');
+//const mongoose = require('mongoose');
 const app = express();
+//connect to mongodb
+const dbURI = 'mongodb+srv://netninja:Hakunamatatta@cluster0/?retryWrites=true&w=majority'
+//mongoose.connect(dbURI,{ useNewUrlParser: true, useUnifiedTopology: true });
 app.listen(3000);
 let path = './';
 app.set('view engine' , 'ejs');
